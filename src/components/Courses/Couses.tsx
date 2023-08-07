cdae8be46dd27785ab0aa08e4e5df12667e20f5e
Marcio-Brust <brust21mw@gmail.com>
2023-08-07 14:07:18 -0300

    couses

--- src/components/Courses/Couses.tsx
+++ src/components/Courses/Couses.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useMedia from "../../utils/hooks/useMedia";
 import { CousesStyle } from "./Couses.style";
 import AOS from "aos";
@@ -11,6 +11,7 @@ import NextIcon from "../Svg/NextIcon";
 import StyledComponentIcon from "../Svg/StyledComponentIcon";
 import TailwindIcon from "../Svg/TailwindIcon";
 import TypescriptIcon from "../Svg/TypescriptIcon";
+import { Popover, Typography } from "@mui/material";
 
 export const Couses = () => {
   const mobile = useMedia("(max-width:50rem)");
@@ -46,6 +47,22 @@ export const Couses = () => {
     },
   ];
 
+  const [name, setName] = useState<string>("");
+
+  const [anchorEl, setAnchorEl] = useState<SVGSVGElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<SVGSVGElement>) => {
+    setAnchorEl(event.currentTarget);
+    setName(event.currentTarget.id);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <CousesStyle aria-expanded={mobile} data-aos="fade-right">
@@ -58,17 +75,79 @@ export const Couses = () => {
               <p>Duração do curso: {item.duracao} horas</p>
             </div>
           ))}
-          <div>
+          <div style={{ padding: "10px" }}>
             <h1>Tecnologias</h1>
-
-            <JavascriptIcon />
-            <TypescriptIcon />
-            <HtmlIcon />
-            <CssIcon />
-            <ReactIcon />
-            <NextIcon />
-            <StyledComponentIcon />
-            <TailwindIcon />
+            <JavascriptIcon
+              style={{ cursor: "pointer" }}
+              id="Javascript"
+              aria-describedby={id}
+              onClick={handleClick}
+            />{" "}
+            <TypescriptIcon
+              style={{ cursor: "pointer" }}
+              id="Typescript"
+              aria-describedby={id}
+              onClick={handleClick}
+            />
+            <HtmlIcon
+              style={{ cursor: "pointer" }}
+              id="HTML"
+              aria-describedby={id}
+              onClick={handleClick}
+            />
+            <CssIcon
+              style={{ cursor: "pointer" }}
+              id="CSS"
+              aria-describedby={id}
+              onClick={handleClick}
+            />
+            <ReactIcon
+              style={{ cursor: "pointer" }}
+              id="React"
+              aria-describedby={id}
+              onClick={handleClick}
+            />
+            <NextIcon
+              style={{ cursor: "pointer" }}
+              id="Nextjs"
+              aria-describedby={id}
+              onClick={handleClick}
+            />
+            <StyledComponentIcon
+              style={{ cursor: "pointer" }}
+              id="Styled component"
+              aria-describedby={id}
+              onClick={handleClick}
+            />
+            <TailwindIcon
+              style={{ cursor: "pointer" }}
+              id="Tailwind"
+              aria-describedby={id}
+              onClick={handleClick}
+            />
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+            >
+              <Typography>
+                <p
+                  style={{
+                    background: "#111",
+                    padding: "10px",
+                    color: "#f1f1f1",
+                    border: "none",
+                  }}
+                >
+                  {name}
+                </p>
+              </Typography>
+            </Popover>
           </div>
         </section>
       </CousesStyle>
