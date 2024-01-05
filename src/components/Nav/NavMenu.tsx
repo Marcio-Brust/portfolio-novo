1c99aecde815d9ca2989ab8713aebca9355e0408
Márcio-Brust <brustmw21@gmail.com>
2024-01-05 09:42:59 -0300

    mudancaNav

--- src/components/Nav/NavMenu.tsx
+++ src/components/Nav/NavMenu.tsx
@@ -1,23 +1,27 @@
 import { NavStyle } from "./NavMenu.style";
 
+const link = [
+  {
+    link: "https://github.com/Marcio-Brust",
+    value: "Github",
+  },
+  {
+    link: "https://www.linkedin.com/in/m%C3%A1rcio-brust-b964501b7",
+    value: "Linkedin",
+  },
+];
+
 export const NavMenu = () => {
   return (
     <>
       <NavStyle>
-        <li>
-          <a href="https://github.com/Marcio-Brust" target="_blanck">
-            Github{" "}
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="https://www.linkedin.com/in/m%C3%A1rcio-brust-b964501b7"
-            target="_blanck"
-          >
-            Linkedin
-          </a>
-        </li>
+        {link.map(({ link, value }) => (
+          <li>
+            <a href={link} target="_blank">
+              {value}
+            </a>
+          </li>
+        ))}
       </NavStyle>
     </>
   );
